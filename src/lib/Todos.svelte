dcc50cba877e6d1e3bc9e2f686687f304bf42f0b
Riley Douglas <rileydouglas@outlook.com>
2022-05-31 12:35:58 +1200

    added todo component inside todos

--- src/lib/Todos.svelte
+++ src/lib/Todos.svelte
@@ -1,23 +1,12 @@
+<script>
+	import Todo from '$lib/Todo.svelte';
+</script>
+
 <!-- List of actual todos -->
 <div class="app-body">
 	<ul>
-		<li class="todo-list list-item-view">
-			<span>
-				<button class="btn btn-done fa-solid fa-square" />
-				<span>First Todo</span>
-			</span>
-			<button class="btn btn-delete fa-solid fa-trash" />
-		</li>
-
-		<li class="todo-list list-item-view completed">
-			<!-- <div class="list-item-view"> -->
-			<span>
-				<button class="btn btn-done"><i class="fa-solid fa-check-square" /></button>
-				<span>First Todo</span>
-			</span>
-			<button class="btn btn-delete fa-solid fa-trash" />
-			<!-- </div> -->
-		</li>
+		<Todo />
+		<Todo />
 	</ul>
 </div>
 
@@ -28,49 +17,9 @@
 		overflow-x: hidden;
 	}
 
-	.todo-list:first-of-type {
-		border-top: 1px solid rgb(121, 121, 121);
-		padding-top: 10px;
-	}
-
-	.list-item-view {
-		padding-top: 5px;
-		padding-bottom: 0px;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-	}
 	ul {
 		list-style-type: none;
 		-webkit-padding-start: 0;
 		padding-left: 0px;
-	}
-
-	.btn {
-		color: inherit;
-		cursor: pointer;
-		font-size: 15px;
-		padding: 10px 12px;
-		border-radius: 2em;
-		background: none;
-		border: 0px solid;
-		transition: 250ms ease-out;
-	}
-
-	.btn:hover {
-		background: #1d2025;
-	}
-
-	.btn:focus {
-		outline: none;
-	}
-
-	.btn-delete {
-		color: #666;
-		font-size: 0.7em;
-	}
-	.completed {
-		color: #6a6f75;
-		opacity: 0.5;
 	}
 </style>
